4c459a8635de88304a5dbc1ac3f32f467a6fe74c
eliveltonsf <elivelton.seven@gmail.com>
2024-02-29 21:41:27 -0300

    refactor: improvement in the application core

--- lib/core/app_colors.dart
+++ lib/core/app_colors.dart
@@ -4,7 +4,7 @@ class AppColors {
   static const Color purple = Color(0xFF8257E5);
   static const Color white = Color(0xFFFFFFFF);
   static const Color black = Color(0xFF514766);
-  static const Color grey = Color(0xFF6E6680);
+  static const Color grey = Color(0xFF808080);
   static const Color lightGrey = Color(0xFFA6A1B2);
   static const Color border = Color(0xFFE1E1E6);
   static const Color primary = Color(0xFFf9579e);
